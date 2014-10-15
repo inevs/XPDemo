20311173d95657795505424587051f1bb5b4ac3d
Sven Günther <sven.guenther@it-agile.de>
2014-10-15 15:21:41 +0200

    Initial Commit

---
+++ XPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XPDemo
+//
+//  Created by Sven Günther on 15.10.14.
+//  Copyright (c) 2014 it-agile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XPDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XPDemo
+//
+//  Created by Sven Günther on 15.10.14.
+//  Copyright (c) 2014 it-agile GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
